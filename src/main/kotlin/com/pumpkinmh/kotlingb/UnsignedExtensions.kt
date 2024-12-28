d9ff2499da935f277571f79a324bcdadd72d6e02
Mabel Hurd <mahurd@email.sc.edu>
2024-12-28 13:04:53 -0500

    Changed UnsignedExtensions.kt to internally convert to UInt() rather than Int()

--- src/main/kotlin/com/pumpkinmh/kotlingb/UnsignedExtensions.kt
+++ src/main/kotlin/com/pumpkinmh/kotlingb/UnsignedExtensions.kt
@@ -1,29 +1,29 @@
 package com.pumpkinmh.kotlingb
 
 infix fun UByte.shr(shiftValue: Int): UByte {
-    var intConversion = this.toInt()
+    var intConversion = this.toUInt()
     intConversion = intConversion.shr(shiftValue)
-    intConversion = intConversion.and(0xFF)
+    intConversion = intConversion.and(0xFFu)
     return intConversion.toUByte()
 }
 
 infix fun UByte.shl(shiftValue: Int): UByte {
-    var intConversion = this.toInt()
+    var intConversion = this.toUInt()
     intConversion = intConversion.shl(shiftValue)
-    intConversion = intConversion.and(0xFF)
+    intConversion = intConversion.and(0xFFu)
     return intConversion.toUByte()
 }
 
 infix fun UShort.shr(shiftValue: Int): UShort {
-    var intConversion = this.toInt()
+    var intConversion = this.toUInt()
     intConversion = intConversion.shr(shiftValue)
-    intConversion = intConversion.and(0xFFFF)
+    intConversion = intConversion.and(0xFFFFu)
     return intConversion.toUShort()
 }
 
 infix fun UShort.shl(shiftValue: Int): UShort {
-    var intConversion = this.toInt()
+    var intConversion = this.toUInt()
     intConversion = intConversion.shl(shiftValue)
-    intConversion = intConversion.and(0xFFFF)
+    intConversion = intConversion.and(0xFFFFu)
     return intConversion.toUShort()
 }
