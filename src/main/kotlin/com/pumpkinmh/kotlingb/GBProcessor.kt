cebfd13d630aa97717ae28af1756133771198eb2
Mabel Hurd <mahurd@email.sc.edu>
2024-12-28 23:56:19 -0500

    Implemented a few load opcodes

--- src/main/kotlin/com/pumpkinmh/kotlingb/GBProcessor.kt
+++ src/main/kotlin/com/pumpkinmh/kotlingb/GBProcessor.kt
@@ -25,4 +25,30 @@ class GBProcessor {
     var stackPointer: UShort = TODO()
     var programCounter: UShort = TODO()
 
+    val memory: GBMemory = TODO()
+
+
+    // Opcodes
+    // Load the value from the register on the right into the register on the left
+    fun LD_r8_r8(targetRegister: ByteRegister, sourceRegister: ByteRegister) {
+        registers[targetRegister.registerIndex] = registers[sourceRegister.registerIndex]
+    }
+
+    // Load the value n8 (from byte 2) into the register r8
+    fun LD_r8_n8(targetRegister: ByteRegister) {
+        val sourceValue = memory[(stackPointer + 1u).toInt()]
+        registers[targetRegister.registerIndex] = sourceValue
+    }
+
+    // Load the value n16 (from bytes 2 and 3) into register r16
+    fun LD_r16_n16(targetRegister: ShortRegister) {
+        val lowerByte = memory[stackPointer + 1u]
+        val upperByte = memory[stackPointer + 2u]
+
+        registers[targetRegister.highRegisterIndex] = upperByte
+        registers[targetRegister.lowRegisterIndex] = lowerByte
+    }
+
+
+
 }
